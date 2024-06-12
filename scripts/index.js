858c340b8be2caae9240566ffb59d0640b413666
Shahbaz Alvi <164075491+Shahbaz1998Alvi@users.noreply.github.com>
2024-06-12 10:42:56 +0530

    Added Bag Count

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,44 @@
-let itemsContainerElement = document.querySelector
-('.items-container');
+let bagItems = [];
+onLoad();
 
-let item = {
-  item_image: 'images/1.jpg',
-  rating: {
-    stars: 4.5,
-    noOfReviews: 1400,
-  },
-  company_name: 'Carlton London',
-  item_name: 'Rhodium-Plated CS Floral Studs',
-  current_price: 606,
-  original_price: 1045,
-  discount_percentage: 45,
+function onLoad ()
+{
+    displayItemsOnHomePage();
+displayBagIcon() 
 }
-itemsContainerElement.innerHTML = `
-<div class="item-container">
-   <img class="item-image" src="${item.item_image}" alt="Item Image">
-    <div class="rating">
-        ${item.rating.stars} ⭐ | ${item.rating.noOfReviews}
-        </div>
-        <div class="company-name">  ${item.company_name} </div>
-        <div class="item-name"> ${item.item_name} </div>
-        <div class="price">
-            <span class="current-price"> ${item.current_price} </span>
-            <span class="original-price"> ${item.original_price} </span>
-            <span class="discount">(${item.discount_percentage}% OFF)</span>
-        </div>
-        <button class="btn-add-bag">Add to Bag</button>
-        </div>`;
+
+function addToBag(itemId) { 
+   bagItems.push(itemId);
+   displayBagIcon();
+}
+
+function displayBagIcon() {
+ let bagItemCountElement = document.querySelector('.bag-item-count');
+ if (bagItems.length > 0) {
+    bagItemCountElement.style.visibility = 'visible';
+ bagItemCountElement.innerText = bagItems.length; 
+} else { 
+        bagItemCountElement.style.visibility = 'hidden';
+}
+}
+function displayItemsOnHomePage() {
+    let itemsContainerElement = document.querySelector('.items-container');
+    let innerHTML = '';
+    items.forEach(item => {
+        innerHTML += `<div class="item-container">
+       <img class="item-image" src="${item.image}" alt="Item Image">
+        <div class="rating">
+            ${item.rating.stars} ⭐ | ${item.rating.count}
+            </div>
+            <div class="company-name">  ${item.company} </div>
+            <div class="item-name"> ${item.item_name} </div>
+            <div class="price">
+                <span class="current-price"> ${item.current_price} </span>
+                <span class="original-price"> ${item.original_price} </span>
+                <span class="discount">(${item.discount_percentage}% OFF)</span>
+            </div>
+            <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button> <!-- corrected parenthesis -->
+            </div>`;
+    });
+    itemsContainerElement.innerHTML = innerHTML;
+}
